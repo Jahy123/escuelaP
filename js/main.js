08042e489aa2b71d69272c86f2b3d75d40498cdc
Jahyire Antunez <jahyireantunezsalazar@gmail.com>
2023-11-17 23:08:59 -0600

    Agrego contenido nuevo

--- js/main.js
+++ js/main.js
@@ -4,42 +4,199 @@ const multimedia = [
     type: "imagen",
     name: "Imagen 1",
     descripcion:
-      "atytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhu",
-    media: "https://via.placeholder.com/150",
+      "Qué privilegio es poder asistir a las clases de la escuela de precursores y aprender más sobre Jehová y su propósito",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275749/IMG-20231117-WA0017_edlwcf.jpg",
   },
   {
     id: 2,
+    type: "imagen",
+    name: "Imagen 2",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275752/IMG-20231117-WA0001_qrckdd.jpg",
+    descripcion:
+      "Los precursores son una bendición para la congregación y para las personas que escuchan el mensaje del Reino",
+  },
+  {
+    id: 11,
     type: "video",
-    name: "Video 1",
-    media: "https://www.example.com/video1.mp4",
+    name: "Video 11",
+    media: "./assets/video/aqu.mp4",
   },
   {
     id: 3,
     type: "imagen",
-    name: "Imagen 2",
-    media: "https://via.placeholder.com/150",
-    descripcion: "guheiughisueh",
+    name: "Imagen 3",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275751/IMG-20231117-WA0000_brfrug.jpg",
+    descripcion:
+      "Eres un colaborador de Jehová. Tu obra de precursor es una participación en el ministerio sagrado que tiene el propósito de salvar vidas",
+  },
+  {
+    id: 9,
+    type: "video",
+    name: "Video 9",
+    media: "./assets/video/dam.mp4",
   },
   {
     id: 4,
     type: "imagen",
-    name: "Imagen 3",
-    media: "https://via.placeholder.com/150",
-    descripcion: "asefeg",
+    name: "Imagen 4",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275754/IMG-20231114-WA0002_x9nl5h.jpg",
+    descripcion:
+      "Los precursores tienen el apoyo y el amor de sus familias, amigos y hermanos de la congregación, que los animan a seguir sirviendo a Jehová con gozo",
+  },
+  {
+    id: 8,
+    type: "video",
+    name: "Video 8",
+    media: "./assets/video/flo.mp4",
   },
   {
     id: 5,
+    type: "imagen",
+    name: "Imagen 5",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275754/IMG-20231115-WA0002_ttcf1l.jpg",
+    descripcion:
+      "Los precursores son felices porque hacen la voluntad de Dios y confían en sus promesas. Saben que él los cuida y los recompensa",
+  },
+  {
+    id: 1,
     type: "video",
-    name: "Video 2",
-    media: "https://www.example.com/video2.mp4",
+    name: "Video 1",
+    media: "./assets/video/oliva.mp4",
   },
   {
     id: 6,
     type: "imagen",
-    name: "Imagen 4",
+    name: "Imagen 6",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275754/IMG-20231115-WA0003_juqwbz.jpg",
+    descripcion:
+      "Los precursores son valientes y no se dejan intimidar por las dificultades o la oposición. Predican con valor y convicción",
+  },
+  {
+    id: 7,
+    type: "imagen",
+    name: "Imagen 7",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275749/IMG-20231117-WA0021_bu5iyt.jpg",
+    descripcion:
+      "Los precursores son humildes y reconocen que necesitan la ayuda de Jehová y su espíritu santo para cumplir su servicio",
+  },
+  {
+    id: 10,
+    type: "video",
+    name: "Video 10",
+    media: "./assets/video/gri.mp4",
+  },
+  {
+    id: 8,
+    type: "imagen",
+    name: "Imagen 8",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275753/IMG-20231115-WA0005_ec7lb7.jpg",
+    descripcion:
+      "Los precursores son agradecidos y expresan su gratitud a Jehová por la oportunidad de servirle como precursores y a los que los apoyan en su servicio",
+  },
+  {
+    id: 7,
+    type: "video",
+    name: "Video 7",
+    media: "./assets/video/neg.mp4",
+  },
+  {
+    id: 9,
+    type: "imagen",
+    name: "Imagen 9",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275753/IMG-20231115-WA0001_mwpj0f.jpg",
+    descripcion:
+      "Los precursores son generosos y comparten lo que tienen con los demás. También comparten su fe y su esperanza con las personas que encuentran",
+  },
+  {
+    id: 6,
+    type: "video",
+    name: "Video 6",
+    media: "./assets/video/jes.mp4",
+  },
+  {
+    id: 10,
+    type: "imagen",
+    name: "Imagen 10",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275752/IMG-20231115-WA0004_p7af31.jpg",
+    descripcion:
+      "Los precursores son ejemplares y siguen el modelo de Jesús, el precursor perfecto. Imitan su amor, su bondad y su obediencia",
+  },
+  {
+    id: 3,
+    type: "video",
+    name: "Video 3",
+    media: "./assets/video/azu.mp4",
+  },
+  {
+    id: 11,
+    type: "imagen",
+    name: "Imagen 11",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275751/IMG-20231117-WA0010_znhuei.jpg",
+    descripcion:
+      "Que Jehová te bendiga por tu servicio fiel y abnegado. Eres una fuente de ánimo y alegría para todos los que te conocen",
+  },
+  {
+    id: 5,
+    type: "video",
+    name: "Video 5",
+    media: "./assets/video/ru.mp4",
+  },
+  {
+    id: 12,
+    type: "imagen",
+    name: "Imagen 12",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275749/IMG-20231117-WA0020_o9hxv2.jpg",
+    descripcion:
+      "No te rindas, sigue adelante con buen ánimo. Jehová es tu ayudante y te dará las fuerzas que necesitas",
+  },
+  {
+    id: 4,
+    type: "video",
+    name: "Video 4",
+    media: "./assets/video/mar.mp4",
+  },
+  {
+    id: 13,
+    type: "imagen",
+    name: "Imagen 13",
+    descripcion:
+      "Te felicitamos por tu decisión de servir a Jehová de todo corazón. El precursorado fortalece tu amistad con Dios y te prepara para el futuro",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275750/IMG-20231117-WA0004_n47lyd.jpg",
+  },
+  {
+    id: 2,
+    type: "video",
+    name: "Video 2",
+    media: "./assets/video/blan.mp4",
+  },
+  {
+    id: 14,
+    type: "imagen",
+    name: "Imagen 14",
+    media:
+      "https://res.cloudinary.com/drqhvncqw/image/upload/v1700275749/IMG-20231117-WA0014_nn80yu.jpg",
     descripcion:
       "Recuerda, cada paso hacia adelante es un testimonio de tu dedicación, y cada obstáculo superado es una oportunidad para crecer aún más. ¡Sigue persiguiendo tus metas con la misma determinación y entusiasmo!",
-    media: "https://via.placeholder.com/150",
+  },
+  {
+    id: 12,
+    type: "video",
+    name: "Video 12",
+    media: "./assets/video/cor.mp4",
+    descripcion: "¡Nos vemos dentro de cinco años!",
   },
 ];
 
@@ -57,6 +214,7 @@ multimedia.forEach((item) => {
   } else if (item.type === "video") {
     const video = document.createElement("video");
     div.className = "video";
+    video.id = "video" + item.id; // Agregado "video" como prefijo para hacer el ID único
     video.src = item.media;
     video.alt = item.name;
     video.controls = true;
@@ -71,4 +229,26 @@ multimedia.forEach((item) => {
   parrafo.textContent = item.descripcion;
   divSeparador.appendChild(parrafo);
   contenedorMultimedia.appendChild(divSeparador);
+});
+
+// Obtén referencias a los elementos de video y audio
+const videos = document.querySelectorAll("video");
+const audioElement = document.getElementById("audio1");
+
+// Escucha el evento de reproducción de cada video
+videos.forEach((video) => {
+  video.addEventListener("play", function () {
+    // Pausa el audio si se está reproduciendo
+    if (!audioElement.paused) {
+      audioElement.pause();
+    }
+  });
+
+  // Escucha el evento de pausa de cada video
+  video.addEventListener("pause", function () {
+    // Reproduce el audio si estaba pausado
+    if (audioElement.paused) {
+      audioElement.play();
+    }
+  });
 });
