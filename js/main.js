1eb8b322b257e930209fa07cb4a4a9fbd87c338e
Jahyire Antunez <jahyireantunezsalazar@gmail.com>
2023-11-17 19:29:56 -0600

    Agrego contenido nuevo

--- js/main.js
+++ js/main.js
@@ -1,25 +1,74 @@
-const fotos = document.getElementsByClassName("foto");
+const multimedia = [
+  {
+    id: 1,
+    type: "imagen",
+    name: "Imagen 1",
+    descripcion:
+      "atytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhuatytyhfgergierhgiergerghurehguierhgiuerhatytyhfgergierhgiergerghurehguierhgiuerhtuhu",
+    media: "https://via.placeholder.com/150",
+  },
+  {
+    id: 2,
+    type: "video",
+    name: "Video 1",
+    media: "https://www.example.com/video1.mp4",
+  },
+  {
+    id: 3,
+    type: "imagen",
+    name: "Imagen 2",
+    media: "https://via.placeholder.com/150",
+    descripcion: "guheiughisueh",
+  },
+  {
+    id: 4,
+    type: "imagen",
+    name: "Imagen 3",
+    media: "https://via.placeholder.com/150",
+    descripcion: "asefeg",
+  },
+  {
+    id: 5,
+    type: "video",
+    name: "Video 2",
+    media: "https://www.example.com/video2.mp4",
+  },
+  {
+    id: 6,
+    type: "imagen",
+    name: "Imagen 4",
+    descripcion:
+      "Recuerda, cada paso hacia adelante es un testimonio de tu dedicación, y cada obstáculo superado es una oportunidad para crecer aún más. ¡Sigue persiguiendo tus metas con la misma determinación y entusiasmo!",
+    media: "https://via.placeholder.com/150",
+  },
+];
 
-for (let i = 0; i < fotos.length; i++) {
-  fotos[i].addEventListener("click", agrandar);
-}
+const contenedorMultimedia = document.getElementById("contenedorMultimedia");
 
-function agrandar() {
-  let foto = this;
-  foto.classList.add("imgZoom");
-  console.log(foto);
+multimedia.forEach((item) => {
+  const div = document.createElement("div");
 
-  // Crear un elemento para el botón "Regresar"
-  let cerrarBoton = document.createElement("div");
-  cerrarBoton.className = "cerrar";
-  cerrarBoton.textContent = "Regresar";
+  if (item.type === "imagen") {
+    const img = document.createElement("img");
+    div.className = "imagen";
+    img.src = item.media;
+    img.alt = item.name;
+    div.appendChild(img);
+  } else if (item.type === "video") {
+    const video = document.createElement("video");
+    div.className = "video";
+    video.src = item.media;
+    video.alt = item.name;
+    video.controls = true;
+    div.appendChild(video);
+  }
 
-  // Agregar un evento clic al botón "Regresar"
-  cerrarBoton.addEventListener("click", function () {
-    foto.classList.remove("imgZoom");
-    foto.removeChild(cerrarBoton);  
-  });
+  contenedorMultimedia.appendChild(div);
 
-  // Agregar el botón "Regresar" al elemento foto
-  foto.appendChild(cerrarBoton);
-}
+  const divSeparador = document.createElement("div");
+  const parrafo = document.createElement("p");
+  divSeparador.className = "parrafo";
+  parrafo.textContent = item.descripcion;
+  divSeparador.appendChild(parrafo);
+  contenedorMultimedia.appendChild(divSeparador);
+});
